865a1a463f0f6d3f4ea4f073f7c53e296b888ed1
rghosh24 <rishiraj.ghosh24@gmail.com>
2022-12-31 17:34:35 -0800

    deploying react app

--- src/components/body/work/index.js
+++ src/components/body/work/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 import "./work.css";
 import Separator from "../../common/separator";
-import {WorkData} from "../../data/work";
+import { WorkData } from "../../data/work";
 import WorkCard from "./work-card";
 function Work() {
   const data = WorkData;
   return (
     <div className="work">
       <Separator />
-      <label className="section-title">Expériences</label>
+      <label className="section-title">Work Experience</label>
       <div className="work-list">
         {data.map((item) => {
           return <WorkCard item={item} />;
         })}
       </div>
     </div>
-  );  
+  );
 }
 
 export default Work;
