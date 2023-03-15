1364fef9e005b8188772e47c435828eb89921f66
rghosh24 <rishiraj.ghosh24@gmail.com>
2023-03-15 17:29:57 -0400

    added snow :)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Home from "./components/home/index";
+import { loadFull } from "tsparticles"
+import Particles from "react-tsparticles"
 
 function App() {
-  return (
+  const init = useCallback(async (engine) => {
+    await loadFull(engine)
+  })
 
+  return (
     <div
       style={{
         backgroundColor: "#d8f9ff",
       }}
     >
+      <Particles options={{
+        particles: {
+          color: {
+            value: "#bebebe"
+          },
+          number: {
+            value: 100
+          },
+          opacity: {
+            value: { min: 0.3, max: 1 }
+          },
+          shape: {
+            type: "circle"
+          },
+          size: {
+            value: { min: 1, max: 5 }
+          },
+          move: {
+            direction: "bottom-right",
+            enable: true,
+            speed: { min: 3, max: 5 },
+            straight: true
+          }
+        }
+      }} init={init} />
       <Home />
-    </div>
+    </div >
   );
 }
 
