e18c1ba331d15cf1dcdb2381885f3d315fabb726
rghosh24 <rishiraj.ghosh24@gmail.com>
2024-01-23 00:14:39 -0500

    mobile header/misc updates

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
             value: { min: 0.3, max: 0.9 }
           },
           shape: {
-            type: window.innerWidth >= 768 ? "circle" : "triangle"
+            type: "circle"
           },
           size: {
             value: { min: 1, max: 4 }
