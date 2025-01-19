78d37f5a061383068413bb03c26dab9926c7f12a
rghosh24 <rishiraj.ghosh24@gmail.com>
2025-01-19 15:26:04 -0800

    build updates

--- src/components/data/skills.js
+++ src/components/data/skills.js
@@ -3,7 +3,7 @@ export const SkillsData = [
     type: "Programming Languages",
     list: [
       {
-        name: "Java, Go, Python, C, SQL, JavaScript, Ruby, Rust, OCaml, HTML, CSS",
+        name: "Java, Go, Python, C, SQL, NoSQL, JavaScript, TypeScript, Ruby, Rust, OCaml, HTML, CSS",
         // icon: <i class="devicon-react-original colored"></i>
       },
     ],
@@ -12,7 +12,7 @@ export const SkillsData = [
     type: "Technologies",
     list: [
       {
-        name: "Kafka, Flink, Spark, React, Docker, Tensorflow, Keras, Pandas, Sklearn, Postman, Git, NodeJS, Linux",
+        name: "Kafka, Flink, Spark, React, GraphQL, Docker, Tensorflow, Keras, Pandas, Sklearn, Postman, Git, NodeJS",
         // icon: <i class="devicon-firebase-plain colored"></i>
       },
     ],
